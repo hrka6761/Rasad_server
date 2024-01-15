e13131c772328824eba18c1e75713f838e87f47c
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-01-15 18:34:42 +0330

    Modifying db_config.js (hardcode config value)

--- utilities/db_config.js
+++ utilities/db_config.js
@@ -1,14 +1,12 @@
-import env from 'dotenv'
 import mysql from 'mysql2'
 
-env.config()
 
 const dbConnectionPool = mysql.createPool(
     {
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD
+        host: 'localhost',
+        database: 'rasaddb',
+        user: 'root',
+        password: 'root'
     }
 ).promise()
 
