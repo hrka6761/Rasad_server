6909975747eb51b5964689a72372563ded0b5119
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-01-16 11:10:50 +0330

    Creating env.js

---
+++ utilities/env.js
@@ -0,0 +1,19 @@
+//Database variables
+const DB_HOST = 'localhost'
+const DB_PORT = '3306'
+const DB_NAME = 'rasaddb'
+const DB_USERNAME = 'root'
+const DB_PASSWORD = 'root'
+
+//names of the database tables
+const USER_TABLE = 'user'
+
+
+export {
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+    DB_USERNAME,
+    DB_PASSWORD,
+    USER_TABLE
+}
