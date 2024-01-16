b3f62a23d0d554f53225f154a4ddf7fe087b71e1
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-01-16 11:15:22 +0330

    Modifying db_config.js (Using env constants instead of hardcode)

--- utilities/db_config.js
+++ utilities/db_config.js
@@ -1,12 +1,14 @@
+import {DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD} from './env.js'
 import mysql from 'mysql2'
 
 
 const dbConnectionPool = mysql.createPool(
     {
-        host: 'localhost',
-        database: 'rasaddb',
-        user: 'root',
-        password: 'root'
+        host: DB_HOST,
+        port: DB_PORT,
+        database: DB_NAME,
+        user: DB_USERNAME,
+        password: DB_PASSWORD
     }
 ).promise()
 
