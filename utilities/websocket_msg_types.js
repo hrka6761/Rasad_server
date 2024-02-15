5b3935cc405c1e531724e7be9659487398c53a54
HAMIDREZA <hrka6761gmail.com>
2024-02-15 07:58:40 +0330

    Creating websocket_msg_types.js

---
+++ utilities/websocket_msg_types.js
@@ -0,0 +1,11 @@
+const msgTypes = {
+    signIn: "SignIn",
+    handshake: "Handshake",
+    call: "Call",
+    answer: "Answer",
+    iceCandidate: "IceCandidate",
+    signOut: "SignOut"
+}
+
+
+export default msgTypes
