c72a98cc089191bc850c3e02f7f4c0c236e49fc5
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-02-19 10:06:09 +0330

    Creating websocket_message_types.js

---
+++ utilities/websocket_message_types.js
@@ -0,0 +1,11 @@
+const msgTypes = {
+    logIn: "LogIn",
+    request: "Request",
+    grant: "Grant",
+    denay: "Denay",
+    logOut: "LogOut",
+    message: "Message"
+}
+
+
+export default msgTypes
